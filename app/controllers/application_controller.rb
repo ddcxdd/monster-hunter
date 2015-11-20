b849cd355770d93ec2d5cd967f52aec0198863ae
kentamakino <kenta@kenta-no-MacBook-Air.local>
2015-11-21 03:11:07 +0900

    no message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,6 @@
 
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
 
-
-
-
-
-=======
   # Prevent  attacks by raising an exception.
   protect_from_forgery with: :exception
 
@@ -19,7 +10,6 @@ class ApplicationController < ActionController::Base
   end
 
   # For APIs, you may want to use :null_session instead.
->>>>>>> 51bd106238ddb45c3a2ec0479a139579da57a5d5
 end
 
 
