e8d4fae5a8017faf0b27e6333cf8dd69ba3f8c8a
ddcxdd <1234567899op@gmail.com>
2015-11-21 02:38:32 +0900

    footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
+  # Prevent  attacks by raising an exception.
+  protect_from_forgery with: :exception
+
+  before_action :set
+  def set
+    @notes = Note.all
+  end
 
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
 end
