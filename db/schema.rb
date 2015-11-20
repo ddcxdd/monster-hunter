0e139a9436fdeb8ec8efc9a429fcc543ede03abc
ddcxdd <1234567899op@gmail.com>
2015-11-20 22:08:19 +0900

    sdawaf

--- db/schema.rb
+++ db/schema.rb
@@ -43,11 +43,9 @@ ActiveRecord::Schema.define(version: 20151120085218) do
   end
 
   create_table "notes", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "title",      limit: 255
-    t.string   "body",       limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string "name",  limit: 255
+    t.string "title", limit: 255
+    t.string "body",  limit: 255
   end
 
 end
