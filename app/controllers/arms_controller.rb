a28fb60af8b228469e9bdff51e9684e1901b83f8
ddcxdd <1234567899op@gmail.com>
2015-11-21 17:19:33 +0900

    buki hyou

--- app/controllers/arms_controller.rb
+++ app/controllers/arms_controller.rb
@@ -1,5 +1,4 @@
 class ArmsController < ApplicationController
-  before_action :set_arm, only: [:show, :edit, :update, :destroy]
 
   # GET /arms
   # GET /arms.json
@@ -9,11 +8,58 @@ class ArmsController < ApplicationController
 
   # GET /arms/1
   # GET /arms/1.json
-  def show
+  def taiken
+    @arms = Arm.all
   end
 
+  def tachi
+    @arms = Arm.all
+  end
 
+  def katate
+    @arms = Arm.all
+  end
+
+  def souken
+    @arms = Arm.all
+  end
+
+  def hummer
+    @arms = Arm.all
+  end
+
+def hue
+  @arms = Arm.all
+end
+
+  def lance
+    @arms = Arm.all
+  end
+
+  def gunlance
+    @arms = Arm.all
+  end
+
+  def slash
+    @arms = Arm.all
+  end
+
+  def light
+    @arms = Arm.all
+  end
+
+  def heavy
+    @arms = Arm.all
+  end
+
+  def yumi
+    @arms = Arm.all
+  end
+
+  def charge
+    @arms = Arm.all
+  end
   private
     # Use callbacks to share common setup or constraints between actions.
-    
+
 end
