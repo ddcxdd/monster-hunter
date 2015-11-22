55caa6359d2ccdec90aa67e4c55ded4e6f76c64b
ddcxdd <1234567899op@gmail.com>
2015-11-22 19:19:26 +0900

    PC対応修正

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,7 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.precompile += %w( pc/application_pc.css )
+Rails.application.config.assets.precompile += %w( pc/arms_pc.css )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
