7d4c030f3eb8acdc6ae0fb68602784309111908d
ddcxdd <1234567899op@gmail.com>
2015-11-22 16:45:17 +0900

    PC対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 
 class ApplicationController < ActionController::Base
-
+  before_action :detect_devise_variant
   # Prevent  attacks by raising an exception.
   protect_from_forgery with: :exception
 
@@ -8,7 +8,16 @@ class ApplicationController < ActionController::Base
   def set
     @notes = Note.all
   end
-
+  
+  private
+  def detect_devise_variant  # (1)と同じ名前
+    devise = request.user_agent
+      if devise.include?("Mobile")
+        else if devise.include?("Android")
+               else request.variant = :PC
+             end
+      end
+  end
   # For APIs, you may want to use :null_session instead.
 end
 
