67d4b6a29c09ea172f18c7085eea19fa817f8c3f
ddcxdd <1234567899op@gmail.com>
2016-03-15 18:36:23 +0900

    aaaa

--- db/migrate/20151124081652_create_hakkens.rb
+++ db/migrate/20151124081652_create_hakkens.rb
@@ -1,19 +1,70 @@
 class CreateHakkens < ActiveRecord::Migration
   def change
-    create_table :hakkens do |t|
-      t.string :quest_number
-      t.string :quest_name
-      t.string :day_night
-      t.string :name
-      t.string :possibility
-      t.string :kiseki_name
-      t.integer :kiseki_number
-      t.string :island
-      t.string :hakken_number
-      t.string :quest_number2
-
-
-      t.timestamps null: false
+    create_table "arms", force: :cascade do |t|
+      t.string   "name",        limit: 255
+      t.string   "skill",       limit: 255
+      t.string   "zokusei",     limit: 255
+      t.integer  "max_level",   limit: 4
+      t.string   "series",      limit: 255
+      t.integer  "attack",      limit: 4
+      t.integer  "max_attack",  limit: 4
+      t.string   "item",        limit: 255
+      t.integer  "item_number", limit: 4
+      t.float    "hyouka",      limit: 24
+      t.string   "comment",     limit: 25
+      t.datetime "created_at",              null: false
+      t.datetime "updated_at",              null: false
+      t.string   "arm_type",    limit: 255
+      t.string   "rare",        limit: 255
+      t.string   "senritu",     limit: 255
     end
+
+    create_table "bougus", force: :cascade do |t|
+      t.string   "name",       limit: 255
+      t.string   "position",   limit: 255
+      t.string   "rare",       limit: 255
+      t.integer  "defence",    limit: 4
+      t.string   "skill",      limit: 255
+      t.string   "wazatama",   limit: 255
+      t.string   "series",     limit: 255
+      t.string   "arm_type",   limit: 255
+      t.integer  "max_level",  limit: 4
+      t.datetime "created_at",             null: false
+      t.datetime "updated_at",             null: false
+    end
+
+    create_table "hakkens", force: :cascade do |t|
+      t.string   "quest_number",  limit: 255
+      t.string   "quest_name",    limit: 255
+      t.string   "day_night",     limit: 255
+      t.string   "name",          limit: 255
+      t.string   "possibility",   limit: 255
+      t.string   "kiseki_name",   limit: 255
+      t.integer  "kiseki_number", limit: 4
+      t.string   "island",        limit: 255
+      t.string   "hakken_number", limit: 255
+      t.string   "quest_number2", limit: 255
+      t.datetime "created_at",                null: false
+      t.datetime "updated_at",                null: false
+    end
+
+    create_table "monsters", force: :cascade do |t|
+      t.string   "name",       limit: 255
+      t.integer  "zokusei",    limit: 4
+      t.datetime "created_at",             null: false
+      t.datetime "updated_at",             null: false
+    end
+
+    create_table "notes", force: :cascade do |t|
+      t.string "name",  limit: 255
+      t.string "title", limit: 255
+      t.text   "body",  limit: 65535
+    end
+
   end
+
+
+
+
+
 end
